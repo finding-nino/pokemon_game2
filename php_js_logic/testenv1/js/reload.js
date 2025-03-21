faa3abb179ef770db9c73b433e56a4760b31f804
BW515609 <515609@vistacollege.nl>
2025-03-21 11:05:56 +0100

    Slight clean up and fixed an edge case

--- php_js_logic/testenv1/js/reload.js
+++ php_js_logic/testenv1/js/reload.js
@@ -1,6 +1,3 @@
-const attackButton = document.getElementById('attack');
-const captureButton = document.getElementById('capture');
-
 function reloadAndDisable() {
     document.querySelector('#body').insertAdjacentHTML('afterend', '<div style="width: 100vw; height: 100vh; position: absolute; top: 0; left: 0;"></div>');
     setTimeout(() => { window.location.reload(); }, 3500);
