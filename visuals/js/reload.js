518e9f40d1fdee2ee802633642e6ca7c81b3487e
BW515609 <515609@vistacollege.nl>
2025-03-27 12:35:33 +0100

    Small cookie fix and name change for site

--- visuals/js/reload.js
+++ visuals/js/reload.js
@@ -1,4 +1,9 @@
+import { deleteCookie } from "./cookieUtils/deleteCookie.js";
+
 function reloadAndDisable() {
+    deleteCookie('captureChance');
+    deleteCookie('currentHP');
+    deleteCookie('maxHP');
     document.querySelector('#body').insertAdjacentHTML('afterend', '<div style="width: 100vw; height: 100vh; position: absolute; top: 0; left: 0;"></div>');
     setTimeout(() => { window.location.reload(); }, 3500);
     
